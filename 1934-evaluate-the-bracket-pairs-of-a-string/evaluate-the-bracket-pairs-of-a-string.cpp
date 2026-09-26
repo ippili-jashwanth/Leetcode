29628fc3d753a0502d754fc2eb03cf7f3ed95bf0
ippili jashwanth <ippilijashwanth@gmail.com>
2026-09-26 19:28:48 +0530

    Time: 419 ms (5.30%) | Memory: 122.2 MB (83.49%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string,string> mp;
+        for(int i = 0; i < knowledge.size(); i++)
+        {
+            mp[knowledge[i][0]] = knowledge[i][1];
+        }
+        for(int i = 0 ; i < s.size(); i++)
+        {
+            string sub = "";
+            if(s[i]=='(')
+            {
+                int ind = i ;
+            
+               int k= ind+1;
+            while(s[k]!=')')
+            {
+                sub+=s[k];
+                k++;
+            }
+            s.erase(ind, k-ind+1);
+            string value="?";
+            if(mp.find(sub)!=mp.end())
+            {
+               value = mp[sub];
+            }
+              s.insert(ind, value); 
+            i= ind + value.size()-1;
+            }
+
+        }
+        return s;
+    }
+};
